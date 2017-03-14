acfdb10a03a414cb04ac6036099d19d6f49f7d72
Ahmad Boissetri Binzagr <abbinzagr@emi.u-bordeaux1.fr>
2017-03-14 10:51:45 +0100

    Maj&add utils

---
+++ utils/modsrc.h
@@ -0,0 +1,19 @@
+#ifndef modsrc_H
+#define modsrc_H
+#include "environ.h"
+typedef struct list_obj noeud;
+
+struct list_obj{
+	union {
+		char* var; /* op & identificateur*/
+		int valeur; /* constante*/
+	}data;
+int type; /* defini le type du noeud "var-cons ou op" */
+struct list_obj* fils_gauche;
+struct list_obj* fils_droit;
+};
+noeud* creerNoeudcons(struct list_obj*nodeg,struct list_obj* nodd,int cons);
+noeud* creerNoeudide(struct list_obj*nodeg,struct list_obj* nodd,char* ide);
+int start(ENV *e,noeud* n);
+#endif
+
